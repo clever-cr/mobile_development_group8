1befbf62a64fa60c12dbfce0a5b50a571b12720d
CynthiaUmuhire <c.umuhire@alustudent.com>
2024-03-10 17:17:14 +0200

    feat: integrate auntentication on the login page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,9 +1,33 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:sharp_mind/components/button.dart';
 import 'package:sharp_mind/components/text_field.dart';
 
 class Login extends StatelessWidget {
   const Login({Key? key}) : super(key: key);
+
+  Future<void> signInWithGoogle() async {
+    try {
+      FirebaseAuth auth = FirebaseAuth.instance;
+      final GoogleSignIn googleSignIn = GoogleSignIn();
+      GoogleSignInAccount? googleUser = await googleSignIn.signIn();
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser!.authentication;
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      final UserCredential userCredential =
+          await auth.signInWithCredential(credential);
+      print(userCredential.user!.displayName);
+      return null;
+    } catch (error) {
+      // Print errors during Google Sign-in
+      print('Error during Google Sign-in: $error');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,6 +54,33 @@ class Login extends StatelessWidget {
               onPressed: () {
                 Navigator.pushNamed(context, '/quiz');
               },
+            ),
+            Text('or continue with'),
+            Container(
+              width: 200,
+              height: 60,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () {
+                      signInWithGoogle();
+                      Navigator.pushNamed(context, '/quiz');
+                    },
+                    child: Container(
+                      child: Image.network(
+                        'http://pngimg.com/uploads/google/google_PNG19635.png',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 5.0,
+                  ),
+                  Text('Sign-in with Google'),
+                ],
+              ),
             ),
           ],
         ),
--- lib/pages/quiz.dart
+++ lib/pages/quiz.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sharp_mind/components/button.dart';
 
@@ -67,6 +69,9 @@ class TextSection extends StatelessWidget {
   const TextSection({super.key});
   @override
   Widget build(BuildContext context) {
+    var currentUser = FirebaseAuth.instance.currentUser;
+    print(currentUser);
+    String userId = "";
     return Container(
       padding: const EdgeInsets.all(32),
       child: Text(
